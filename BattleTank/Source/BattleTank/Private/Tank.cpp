dd347e95fa56edfc3beddb9d0d4d415843f8e13e
Hai Nguyen <tigerclaws12894@gmail.com>
2016-09-26 02:36:23 +0700

    BP load order changed

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -16,6 +16,9 @@ ATank::ATank()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Donkey: Tank C++ Construct"), *TankName);
 }
 
 // Called when the game starts or when spawned
